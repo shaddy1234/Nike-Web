2a5ea0119e8f636159704360f8d06244330d6800
Shadrack Kimaau <musyokishadrach1@gmail.com>
2025-02-13 14:50:06 +0300

    Import the sections

--- Nike/src/App.jsx
+++ Nike/src/App.jsx
@@ -1,15 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Nav } from "./components";
+import {
+  CustomerReviews,
+  Footer,
+  Hero,
+  PopularProducts,
+  Services,
+  SpecialOffer,
+  Subscribe,
+  SuperQuality,
+} from "./sections";
 
-function App() {
-
+const App = () => {
   return (
-    <div className="flex items-center justify-center h-screen bg-blue-500 text-white text-3xl font-bold">
-      Tailwind CSS is working! 🎉
-    </div>
-  )
-}
+    <main className='relative'>
+      <Nav />
+      <section className='xl:padding-l wide:padding-r padding-b'>
+        <Hero />
+      </section>
+      <section className='padding'>
+        <PopularProducts />
+      </section>
+      <section className='padding'>
+        <SuperQuality />
+      </section>
+      <section className='padding-x py-10'>
+        <Services />
+      </section>
+      <section className='padding'>
+        <SpecialOffer />
+      </section>
+      <section className='bg-pale-blue padding'>
+        <CustomerReviews />
+      </section>
+      <section className='padding-x sm:py-32 py-16 w-full'>
+        <Subscribe />
+      </section>
+      <section className=' bg-black padding-x padding-t pb-8'>
+        <Footer />
+      </section>
+    </main>
+  );
+};
 
-export default App
+export default App;
