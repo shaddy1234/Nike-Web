aeee585857cab250a352cf1e90a9ce4eac5b3817
Shadrack Kimaau <musyokishadrach1@gmail.com>
2025-02-13 12:31:59 +0300

    Set up

--- Nike/src/App.jsx
+++ Nike/src/App.jsx
@@ -1,10 +1,15 @@
 import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
+
   return (
     <div className="flex items-center justify-center h-screen bg-blue-500 text-white text-3xl font-bold">
       Tailwind CSS is working! 🎉
     </div>
-  );
+  )
 }
-export default App;
+
+export default App
