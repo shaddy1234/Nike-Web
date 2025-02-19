f6e7d9da6de3dde9b6f039866644138be12f6463
Shadrack Kimaau <musyokishadrach1@gmail.com>
2025-02-19 09:55:05 +0300

    Fix navbar

--- Nike/src/Components/Nav.jsx
+++ Nike/src/Components/Nav.jsx
@@ -20,7 +20,7 @@ const Nav = () => {
             <li key={item.label}>
               <a
                 href={item.href}
-                className='font-montserrat leading-normal text-lg text-slate-gray'
+                className='font-montserrat leading-normal text-lg text-slate-500'
               >
                 {item.label}
               </a>
