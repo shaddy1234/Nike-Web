99eef03f32daae8c4709e6cd69d73a91925d03ca
Shadrack Kimaau <musyokishadrach1@gmail.com>
2025-02-19 19:36:59 +0300

    Fix button

--- Nike/src/Components/Button.jsx
+++ Nike/src/Components/Button.jsx
@@ -29,3 +29,32 @@ const Button = ({
   };
   
   export default Button;
+
+
+// const Button = ({
+//   label,
+//   iconURL = "",
+//   backgroundColor = "bg-red-500",
+//   textColor = "text-white",
+//   borderColor = "border-red-500",
+//   fullWidth = false,
+// }) => {
+
+//   return (
+//     <button
+//       className={`relative z-50 p-4 border border-black ${backgroundColor} ${textColor} ${borderColor} ${fullWidth ? "w-full" : ""}`}
+//     >
+//       {label}
+
+//       {iconURL && (
+//         <img
+//           src={iconURL}
+//           alt="button icon"
+//           className="ml-2 w-5 h-5"
+//         />
+//       )}
+//     </button>
+//   );
+// };
+
+// export default Button;
