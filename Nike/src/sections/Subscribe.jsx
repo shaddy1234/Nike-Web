01d164f5d4c390b15581a477c6a52deccb3fbb42
Shadrack Kimaau <musyokishadrach1@gmail.com>
2025-02-19 19:38:25 +0300

    Fix subcscibe bug

--- Nike/src/sections/Subscribe.jsx
+++ Nike/src/sections/Subscribe.jsx
@@ -13,7 +13,15 @@ const Subscribe = () => {
       <div className='lg:max-w-[40%] w-full flex items-center max-sm:flex-col gap-5 p-2.5 sm:border sm:border-slate-gray rounded-full'>
         <input type='text' placeholder='subscribe@nike.com' className='input' />
         <div className='flex max-sm:justify-end items-center max-sm:w-full'>
-          <Button label='Sign Up' fullWidth />
+          {/* <Button label='Sign Up' fullWidth /> */}
+          <Button
+          label="Sign Up"
+          iconURL=""
+          backgroundColor="bg-blue-700"
+          textColor="text-white"
+          borderColor="border-white"
+          fullWidth
+        />
         </div>
       </div>
     </section>
